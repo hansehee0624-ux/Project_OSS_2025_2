3384d04280e08080df77e2c418132a5f73db7324
SeHee Han <hansehee0624@gmail.com>
2025-12-01 23:28:32 +0900

    첫번째 커밋
    [기능추가] 히스토리 창 오른쪽에 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -12,6 +12,17 @@ class Calculator:
         # 입력창
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
+        
+        #새로 추가된 코드1 >> 히스토리 리스트 박스 생성
+        self.history = tk.Listbox(root, font=("Arial", 12))
+        self.history.pack(side="right", fill="both", padx=5)
+        
+        #새로 추가된 코드2 >> 스크롤 추가
+        scroll = tk.Scrollbar(root)
+        scroll.pack(side="right", fill="y")
+
+        self.history.config(yscrollcommand=scroll.set)
+        scroll.config(command=self.history.yview)
 
         # 버튼 생성
         buttons = [
@@ -38,8 +49,14 @@ class Calculator:
         if char == 'C':
             self.expression = ""
         elif char == '=':
+        
+        #새로 추가된 코드3 >> 이전 계산기록 수식 저장
+        old_exp = self.expression
+        
             try:
                 self.expression = str(eval(self.expression))
+                #새로 추가된 코드4 >> 리스트 박스에 기록추가
+                self.history.insert(tk.END, f"{old_exp} = {self.expression}")
             except Exception:
                 self.expression = "에러"
         else:
